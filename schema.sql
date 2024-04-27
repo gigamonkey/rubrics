fe44e4f384f05d7a302379ee08da4eac283d18d8
Peter Seibel <peter@gigamonkeys.com>
2024-04-27 08:54:28 -0700

    Schema with submissionId

--- schema.sql
+++ schema.sql
@@ -1,29 +1,44 @@
--- Answers from answers.json at a particular date and commit
+-- Save answers from answers.json at a particular date and commit
 
-PRAGMA foreign_keys = ON;
+--PRAGMA foreign_keys = ON;
 
--- Answers per student, date, commit
-drop table if exists answers;
-create table answers (
-  github TEXT NOT NULL,
-  sha TEXT NOT NULL,
-  question TEXT NOT NULL,
-  answer TEXT NOT NULL
+-- Questions for ordering
+drop table if exists questions;
+create table questions (
+  question TEXT PRIMARY_KEY,
+  sequence INTEGER
 );
 
 -- Rubric items per question
 drop table if exists rubric;
 create table rubric (
+  rubric_id INTEGER PRIMARY KEY,
   question TEXT NOT NULL,
   criteria TEXT NOT NULL,
-  weight NUMBER
+  sequence INTEGER NOT NULL,
+  weight NUMBER NOT NULL
+);
+
+-- Submissions per student, date commit
+drop table if exists submissions;
+create table submissions (
+  submission_id INTEGER PRIMARY KEY,
+  github TEXT NOT NULL,
+  date TEXT NOT NULL,
+  sha TEXT NOT NULL
+);
+
+-- Answers per student, date, commit
+drop table if exists answers;
+create table answers (
+  submission_id INTEGER,
+  question TEXT NOT NULL,
+  answer TEXT NOT NULL
 );
 
 drop table if exists scores;
 create table scores (
-  github TEXT NOT NULL,
-  date TEXT NOT NULL,
-  sha TEXT NOT NULL,
+  submission_id INTEGER,
   question TEXT NOT NULL,
   criteria TEXT NOT NULL,
   score INTEGER NOT NULL
