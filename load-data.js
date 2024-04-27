95a0ce1ffc36a567565a85ff112a14d990d25230
Peter Seibel <peter@gigamonkeys.com>
2024-04-27 08:53:11 -0700

    Load submissions with submissionId

--- load-data.js
+++ load-data.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { glob } from 'glob';
 import { DB } from './db.js';
 
-const db = new DB("db.db");
+const db = new DB('db.db', 'schema.sql');
 
 const rubric = YAML.parse(fs.readFileSync('rubric.yml', 'utf8'));
 
@@ -17,19 +17,24 @@ const getDate = (p) => path.basename(path.dirname(p));
 const answerFiles = await glob("answers/**/answers.json", {});
 
 // Load the rubric into the database
-Object.entries(rubric).forEach(([question, items]) => {
-  Object.entries(items).forEach(([criteria, weight]) => {
-    db.insertRubricItem({question, criteria, weight});
+Object.entries(rubric).forEach(([question, items], sequence) => {
+  db.insertQuestion({sequence, question});
+  Object.entries(items).forEach(([criteria, weight], sequence) => {
+    db.insertRubricItem({question, criteria, sequence, weight});
   });
 });
 
 // Load answers into the database
 answerFiles.forEach(f => {
+
   const github = getGithub(f);
+  const date = getDate(f);
   const sha = fs.readFileSync(path.join(path.dirname(f), "commit.txt"), { encoding: 'utf8' }).trim();
   const answers = JSON.parse(fs.readFileSync(f, { encoding: 'utf8' })).map(a => a?.trim() ?? "");
+
+  const submissionId = db.insertSubmission({github, date, sha}).lastInsertRowid;
   answers.forEach((answer, i) => {
     const question = questions[i];
-    db.insertAnswer({ github, sha, question, answer });
+    db.insertAnswer({ submissionId, question, answer });
   });
 });
