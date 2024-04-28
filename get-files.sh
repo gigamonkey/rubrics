4cd8111613db45a04959b06fc41defcea6d08143
Peter Seibel <peter@gigamonkeys.com>
2024-04-28 08:47:45 -0700

    Inheritance 2 spicy setup.

--- get-files.sh
+++ get-files.sh
@@ -5,4 +5,4 @@ GITHUB_DIR=~/teaching/csa/github/
 dir=$(realpath --relative-to="$GITHUB_DIR" $(pwd))
 
 cd "$GITHUB_DIR"
-./repos get-files-by-day.sh main assessments/inheritance-2-mild/answers.json $dir/answers/
+./repos get-files-by-day.sh main assessments/inheritance-2-spicy/answers.json $dir/answers/
