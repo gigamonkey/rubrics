a5e0d9c1b80718e88a124382ad27065c1dfd4080
Peter Seibel <peter@gigamonkeys.com>
2024-05-02 16:46:59 -0700

    Setting up for new directory structure.

--- get-files.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-GITHUB_DIR=~/teaching/csa/github/
-
-dir=$(realpath --relative-to="$GITHUB_DIR" $(pwd))
-
-cd "$GITHUB_DIR"
-./repos get-files-by-day.sh main assessments/inheritance-2-spicy/answers.json $dir/answers/
